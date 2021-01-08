5bee1efecdb80eb009b81f6e42455d53e2a4c63f
nnakamura95 <we.are.one779@gmail.com>
2021-01-08 16:35:42 +0900

    inital commit

---
+++ nutri-api/src/main/java/com/example/nutri/NutriApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriApplication.class, args);
+	}
+
+}
